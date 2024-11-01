aec91e051e9fd019c20522b57d1de43352844f08
Mikhail Somov <mikhail.somov@laniakea-continuum.com>
2024-11-01 21:34:03 +0300

    Sprint 04 task

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,72 +1,222 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
-    // MARK: - Lifecycle
+    
+    @IBOutlet weak private var imageView: UIImageView!
+    @IBOutlet weak private var textLabel: UILabel!
+    @IBOutlet weak private var counterLabel: UILabel!
+    @IBOutlet weak private var yesButton: UIButton!
+    @IBOutlet weak private var noButton: UIButton!
+    
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+        disableButtons()
+        animateButtonPress(sender)
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
+        
+        if givenAnswer == currentQuestion.correctAnswer {
+            correctAnswers += 1
+        }
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+    }
+    
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+        disableButtons()
+        animateButtonPress(sender)
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = false
+        
+        if givenAnswer == currentQuestion.correctAnswer {
+            correctAnswers += 1
+        }
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        
+    }
+    
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false)
+    ]
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let questionStep = QuizStepViewModel(
+            image: UIImage(named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+        return questionStep
+    }
+    
+    private func show(quiz step: QuizStepViewModel) {
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
+    }
+    
+    private func showAnswerResult(isCorrect: Bool) {
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.imageView.layer.borderWidth = 0
+            self.showNextQuestionOrResult()
+        }
+    }
+    
+    private func showNextQuestionOrResult() {
+        if currentQuestionIndex == questions.count - 1 {
+            showFireworksAnimation()
+            let alert = UIAlertController(
+                title: "Этот раунд окончен",
+                message: "Ваш результат \(correctAnswers) из \(questions.count)",
+                preferredStyle: .alert
+            )
+            
+            let action = UIAlertAction(title: "Начать заново", style: .default) { _ in
+                self.currentQuestionIndex = 0
+                self.correctAnswers = 0
+                
+                let firstQuestion = self.questions[self.currentQuestionIndex]
+                let viewModel = self.convert(model: firstQuestion)
+                self.show(quiz: viewModel)
+            }
+            
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+            
+        } else {
+            currentQuestionIndex += 1
+            
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            
+            show(quiz: viewModel)
+        }
+    }
+    
+    private func disableButtons() {
+        yesButton.isUserInteractionEnabled = false
+        noButton.isUserInteractionEnabled = false
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.yesButton.isUserInteractionEnabled = true
+            self.noButton.isUserInteractionEnabled = true
+        }
+    }
+    
+    private func showFireworksAnimation() {
+        let fireworksEmitter = CAEmitterLayer()
+        fireworksEmitter.emitterPosition = CGPoint(x: view.bounds.width / 2, y: -50)
+        fireworksEmitter.emitterSize = CGSize(width: view.bounds.width, height: 0)
+        fireworksEmitter.emitterShape = .line
+
+        let cell = CAEmitterCell()
+        
+        cell.contents = UIImage(systemName: "circle.fill")?.cgImage
+        cell.color = UIColor.systemPink.cgColor
+        cell.birthRate = 6
+        cell.lifetime = 4.0
+        cell.velocity = 300
+        cell.velocityRange = 100
+        cell.emissionLongitude = .pi
+        cell.emissionRange = .pi / 4
+        cell.scale = 0.1
+        cell.scaleRange = 0.05
+        cell.alphaSpeed = -0.1
+        cell.yAcceleration = 150
+        cell.spin = 2
+        cell.spinRange = 3
+
+        fireworksEmitter.emitterCells = [cell]
+        view.layer.addSublayer(fireworksEmitter)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            fireworksEmitter.removeFromSuperlayer()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.layer.cornerRadius = 20
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 0
+        
+        let firstQuestion = questions[currentQuestionIndex]
+        let viewModel = convert(model: firstQuestion)
+        show(quiz: viewModel)
+        
     }
+    
 }
 
-/*
- Mock-данные
- 
- 
- Картинка: The Godfather
- Настоящий рейтинг: 9,2
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Dark Knight
- Настоящий рейтинг: 9
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Kill Bill
- Настоящий рейтинг: 8,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Avengers
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Deadpool
- Настоящий рейтинг: 8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: The Green Knight
- Настоящий рейтинг: 6,6
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: ДА
- 
- 
- Картинка: Old
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: The Ice Age Adventures of Buck Wild
- Настоящий рейтинг: 4,3
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: Tesla
- Настоящий рейтинг: 5,1
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
- 
- 
- Картинка: Vivarium
- Настоящий рейтинг: 5,8
- Вопрос: Рейтинг этого фильма больше чем 6?
- Ответ: НЕТ
-*/
+private func animateButtonPress(_ button: UIButton) {
+    UIView.animate(withDuration: 0.1,
+                   animations: {
+        button.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+    },
+                   completion: { _ in
+        UIView.animate(withDuration: 0.1) {
+            button.transform = CGAffineTransform.identity
+        }
+    })
+}
+
+private struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
+
+private struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
+
+private struct QuizResultViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
