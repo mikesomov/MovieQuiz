cda6be1a7541a28f0a4803326000cbf8d4893c85
Mikhail Somov <mikhail.somov@laniakea-continuum.com>
2024-12-09 06:09:35 +0000

    Refactoring part two - Trying to apply tests

---
+++ MovieQuiz/Services/MovieQuizViewControllerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizViewControllerProtocol.swift
+//  MovieQuiz
+//
+//  Created by Mike Somov on 09.12.2024.
+//
+
+import Foundation
---
+++ MovieQuizViewControllerMock/MovieQuizViewControllerMock.swift
@@ -0,0 +1,16 @@
+//
+//  MovieQuizViewControllerMock.swift
+//  MovieQuizViewControllerMock
+//
+//  Created by Mike Somov on 09.12.2024.
+//
+
+import Testing
+
+struct MovieQuizViewControllerMock {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
